bc211fbb53b2816e36ed042e844acd4a1b830691
Kostis Triantafyllakis <ctriant@admin.grnet.gr>
2022-04-08 20:36:59 +0300

    Change logo path

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import setThemeValue from "../helpers/helpers";
 import { Navbar, Nav, Container, Image, Row, Col } from "react-bootstrap";
 
 import {THEME} from "../config";
-import logo from "../assets/logo.svg";
+// import logo from "../assets/logo.svg";
 import styles from "../styles/App.module.css";
 
 const Header = () => {
@@ -17,7 +17,7 @@ const Header = () => {
         <Navbar.Brand href="/">
           <Row className="align-items-baseline g-1">
             <Col>
-              <Image src={logo} className={`${styles["navbar-logo"]} border-end`}/>
+              <Image src={"/logo.svg"} className={`${styles["navbar-logo"]} border-end`}/>
             </Col>
             <Col>
               <span className={`text-white`}>Status</span>
